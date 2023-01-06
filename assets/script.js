92f8d481723bdf3555fdec6fd26d5fc13ece5d30
xarli <117117311+c4rli@users.noreply.github.com>
2023-01-06 22:36:19 +0000

    Add input validation

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,8 @@ var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+var charTypes = [false, false, true, false];
+
 var allowedCharacters = {
   specialCharacters: specialCharacters,
   numericCharacters: numericCharacters,
@@ -21,70 +23,14 @@ function randomizer(limit) {
   return Math.floor(Math.random() * limit);
 }
 
-function returnCharacter(type) {
+// Function for getting a random element from an array
+function getCharacter(type) {
   var charArray = Object.values(allowedCharacters)[type];
   return charArray[randomizer(charArray.length)];
 }
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-var charTypes = [false, false, true, false];
-
-function typeCheck(array) {
-  var selectedTypes = [];
-
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === true) {
-      selectedTypes.push(i);
-    }
-  }
-  console.log(selectedTypes);
-  return selectedTypes;
-}
-
-function generateString(length) {
-  var passwordStringArr = [];
-
-  for (var i = 0; i < length; i++) {
-    var options = typeCheck(charTypes);
-    var charType = options[randomizer(options.length)];
-    console.log(charType);
-    var character = returnCharacter(charType);
-    console.log(character);
-    passwordStringArr.push(character)
-  }
-  
-  passwordString= passwordStringArr.join("");
-  console.log(passwordString);
-
-  return passwordString;
-}
-
-
-
-
-// console.log(typeCheck(charTypes));
-
-
-
-
-// input true false for each char type 
-// make an array that has indexes of types of characters 
-
-
-
-// Function to generate password with user input
-function generatePassword() {
-  console.log(charTypes);
-
   charTypes[0] = document.getElementById('checkSpecial').checked;
   console.log(document.getElementById('checkSpecial').checked);
   charTypes[1] = document.getElementById('checkNumbers').checked;
@@ -94,9 +40,57 @@ function generatePassword() {
   charTypes[3] = document.getElementById('checkUpper').checked;
   console.log(document.getElementById('checkUpper').checked)
 
+  return charTypes.find(function(x){
+    return x === true;
+  })
+}
+
+function typeCheck(array) {
+  var selectedTypes = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === true) {
+      selectedTypes.push(i);
+    }
+  }
+  console.log(selectedTypes);
+  return selectedTypes;
+}
+
+function getPasswordString(length) {
+  var passwordStringArr = [];
+
+  for (var i = 0; i < length; i++) {
+    var options = typeCheck(charTypes);
+    var charType = options[randomizer(options.length)];
+    console.log(charType);
+    var character = getCharacter(charType);
+    console.log(character);
+    passwordStringArr.push(character)
+  }
+  
+  var passwordString = passwordStringArr.join("");
+  console.log(passwordString);
+
+  return passwordString;
+}
+
+function validateInput(){
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  var passwordLength = prompt("How many characters?");
+  if ((passwordLength < 10) || (passwordLength > 64)){
+    return 1;
+  }
 
   console.log(charTypes);
-  return generateString(prompt("How many characters?"));
+  if (getPasswordOptions() === undefined){
+    return 2;
+  }
+
+  return getPasswordString(passwordLength);
 }
 
 // Get references to the #generate element
@@ -105,9 +99,18 @@ var generateBtn = document.querySelector('#generate');
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  if (password === 1){
+    alert("Incorrect length.");
+  }
+
+  else if (password === 2){
+    alert("No characters selected.");
+  }
+  else {
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
