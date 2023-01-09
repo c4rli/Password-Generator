529de5d3f7956b007613c75057cff7f409602578
xarli <117117311+c4rli@users.noreply.github.com>
2023-01-09 21:07:14 +0000

    Fix password validation to include at least one of each char type

--- assets/script.js
+++ assets/script.js
@@ -64,37 +64,42 @@ function typeCheck(array) {
 }
 
 function generatePasswordString(length) {
-  
+
 }
 
 function getPasswordString(length) {
-  var passwordStringArr = [];
-  var charComparison = [];
-
-  for (var i = 0; i < length; i++) {
-    var options = typeCheck(charTypes);
-    var charType = options[randomizer(options.length)];
-    console.log(charType);
-    var character = getCharacter(charType);
-    console.log(character);
-    passwordStringArr.push(character)
-  }
-
-  for (var i = 0; i < Object.values(allowedCharacters).length; i++) {
-    // console.log(array[1])
-    console.log(Object.values(allowedCharacters)[i][0]);
-    charComparison.push(findCommonElements3(passwordStringArr, Object.values(allowedCharacters)[i][1]));
-  }
-
-  console.log(charTypes);
-  console.log(charComparison);
 
 
-  if (charComparison.join("") != charTypes.join("")) {
-    var passwordString = passwordStringArr.join("");
-    console.log(passwordString);
-    getPasswordString(length);
-  }
+  do {
+    var passwordStringArr = [];
+    var charComparison = [];
+    for (var i = 0; i < length; i++) {
+      var options = typeCheck(charTypes);
+      var charType = options[randomizer(options.length)];
+      console.log(charType);
+      var character = getCharacter(charType);
+      console.log(character);
+      passwordStringArr.push(character)
+    }
+
+
+
+    for (var i = 0; i < Object.values(allowedCharacters).length; i++) {
+      // console.log(array[1])
+      console.log(Object.values(allowedCharacters)[i][0]);
+      charComparison.push(findCommonElements3(passwordStringArr, Object.values(allowedCharacters)[i][1]));
+    }
+      console.log(charTypes);
+      console.log(charComparison);
+  } while (charComparison.join("") != charTypes.join(""));
+
+
+
+  // if (charComparison.join("") != charTypes.join("")) {
+  //   var passwordString = passwordStringArr.join("");
+  //   console.log(passwordString);
+  //   getPasswordString(length);
+  // }
 
 
   var passwordString = passwordStringArr.join("");
@@ -122,13 +127,16 @@ function findCommonElements3(arr1, arr2) {
 function generatePassword() {
   // var passwordLength = prompt("How many characters?");
   var passwordLength = document.getElementById('noOfChars').value;
-  if ((passwordLength < 10) || (passwordLength > 64)) {
+  
+  if (!Number.isInteger(passwordLength)) {
     return 1;
   }
-
+  else if ((passwordLength < 10) || (passwordLength > 64)) {
+    return 2;
+  }
   // console.log(charTypes);
   if (getPasswordOptions() === undefined) {
-    return 2;
+    return 3;
   }
 
   return getPasswordString(passwordLength);
@@ -138,12 +146,17 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   if (password === 1) {
-    alert("Incorrect length.");
+    alert("Invalid value");
   }
 
   else if (password === 2) {
+    alert("Incorrect length");
+  }
+  else if (password === 3) {
     alert("No characters selected.");
   }
+
+
   else {
     var passwordText = document.querySelector('#password');
     passwordText.value = password;
@@ -155,3 +168,32 @@ var generateBtn = document.querySelector('#generate');
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+
+
+function myFunction()
+{
+    var x = document.getElementById("password"),
+        txt = x.innerHTML;
+    (myFunction = function() {
+        var newText = "";
+        for(var i=0, l=txt.length; i<l; i++)
+        {
+            newText += '<span style="color:#'+getColor()+'">'+txt.charAt(i)+'</span>';
+        }
+        x.innerHTML = newText;
+    })();
+}
+
+function getColor()
+{
+    var colorString="";
+    for(var i=0;i<6;i++)
+    {
+        var num = Math.floor(Math.random()*17);
+        hexNum = num.toString(16);
+        colorString += hexNum;
+    }
+    return colorString;
+}
